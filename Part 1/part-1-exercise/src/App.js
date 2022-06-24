a23c51c7a071a3aeead31588b52c39a494361fe8
Semih TULGAR <semihtulgar@gmail.com>
2022-06-24 14:06:45 +0300

    Course Information | Exercises 1.4 step 4 has completed

--- Part 1/part-1-exercise/src/App.js
+++ Part 1/part-1-exercise/src/App.js
@@ -27,7 +27,7 @@ function Total(props) {
   for (let i = 0; i < props.parts.length; i++) {
     total += props.parts[i].exercises;
   }
-  
+
   return (
     <p>Number of exercises {total}</p>
   )
@@ -37,24 +37,26 @@ function Total(props) {
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total parts={[part1, part2, part3]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
