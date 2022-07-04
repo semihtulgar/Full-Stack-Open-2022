75987bfce48f3f54d971515421f640f21dd08895
Semih TULGAR <semihtulgar@gmail.com>
2022-07-04 21:33:51 +0300

    Part 2 | The Phonebook step3 has completed

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,25 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number : "040-123-45-67" }]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newPerson = {name : newName}
-    const personExist = persons.find(person => person.name == newPerson.name);
-    if (personExist === undefined) {
-      setPersons(persons.concat(newPerson));      
-      setNewName('');
-    }else{
-      alert(`${newName} is already added to phonebook`);
-    }
-  }
 
-  const handleChange = (e) => {
-    setNewName(e.target.value)
+    const newPerson = { name: newName, number: newNumber };
+    const personExist = persons.find((person) => person.name === newPerson.name || person.number === newPerson.number);
+    
+    if (personExist === undefined) {
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+      setNewNumber("");
+    } else {
+      alert(`this contact information is already added to phonebook`);
+    }
+  };
+
+  const handleName = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumber = (e) => {
+    setNewNumber(e.target.value);
   }
 
   return (
@@ -27,16 +33,21 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input onChange={handleName} value={newName}/>
+        </div>
+        <div>
+          number: <input onChange={handleNumber} value={newNumber} type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}" placeholder="123-456-78-90" />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => (
+        <p key={person.name}>{person.name} {person.number}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default App;
